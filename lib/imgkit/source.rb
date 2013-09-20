b3ba7560dea403abc20f84e64e510de9dfe02433
Claudio Baccigalupo <claudio@topspinmedia.com>
2013-09-20 12:11:10 -0700

    Recognize that `TempFile` is a file.
    The problem was that, since `wkhtmltoimage` [does not work with HTML in STDIN](http://code.google.com/p/wkhtmltopdf/issues/detail?id=534),
    the HTML must be stored in a temporary file, and the original IMGKit thinks that
    [Tempfile instances are not files](https://github.com/csquared/IMGKit/blob/master/lib/imgkit/source.rb#L14).

--- lib/imgkit/source.rb
+++ lib/imgkit/source.rb
@@ -1,27 +1,27 @@
 class IMGKit
-  
+
   class Source
-        
+
     def initialize(url_file_or_html)
       @source = url_file_or_html
     end
-    
+
     def url?
       @source.is_a?(String) && @source.match(/\Ahttp/)
     end
-    
+
     def file?
-      @source.kind_of?(File)
+      @source.kind_of?(File) || @source.kind_of?(Tempfile)
     end
-    
+
     def html?
       !(url? || file?)
     end
-    
+
     def to_s
       file? ? @source.path : @source
     end
-    
+
   end
-  
+
 end
